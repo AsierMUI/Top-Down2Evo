a3f16e51eae046fb195c3e15f867d1c2e4a85f89
AsierMUI <asieravino@gmail.com>
2025-01-08 12:10:15 +0100

    personaje/script/ sprites

---
+++ Top-Down2evo/Assets/New Folder/Scripts/PruebaMovimiento.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PruebaMovimiento : MonoBehaviour
+{
+
+    //Hacer apuntes de todo
+    //preguntar a Jorge
+    public float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+    }
+}
