e43a51b6e9d5be233e954f3c868779e81feae6ef
AsierMUI <asieravino@gmail.com>
2025-02-12 16:02:29 +0100

    a

--- Top-Down2evo/Assets/New Folder/Scripts/PruebaMovimiento.cs
+++ Top-Down2evo/Assets/New Folder/Scripts/PruebaMovimiento.cs
@@ -16,6 +16,8 @@ public class PruebaMovimiento : MonoBehaviour
     private float movimientoY;
     private Animator animator;
 
+    bool isAttacking = false;
+
 
 
     void Start()
@@ -28,19 +30,19 @@ public class PruebaMovimiento : MonoBehaviour
 
     void Update()
     {
-        movimientoX = Input.GetAxisRaw("Horizontal");
-        movimientoY = Input.GetAxisRaw("Vertical");
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
 
-        animator.SetFloat("MovimientoX", movimientoX);
-        animator.SetFloat("MovimientoY", movimientoY);
-
-        if(movimientoX != 0 || movimientoY != 0)
-        {
-            animator.SetFloat("UltimoX", movimientoX);
-            animator.SetFloat("UltimoY", movimientoY);
+        //ataque
+        if (Input.GetKeyDown(KeyCode.RightAlt))
+            {
+            animator.Play("Ataque");
+            isAttacking = true;
         }
 
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        Movimiento();
+
+
+
     }
 
     #region Input Methods
@@ -51,6 +53,27 @@ public class PruebaMovimiento : MonoBehaviour
 
     #endregion
 
+    void Movimiento()
+    {
+        //animacion movimiento
+        if (isAttacking) return;
 
+        movimientoX = Input.GetAxisRaw("Horizontal");
+        movimientoY = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("MovimientoX", movimientoX);
+        animator.SetFloat("MovimientoY", movimientoY);
+
+        if (movimientoX != 0 || movimientoY != 0)
+        {
+            animator.SetFloat("UltimoX", movimientoX);
+            animator.SetFloat("UltimoY", movimientoY);
+        }
+    }
+
+    void FinAtaque()
+    {
+        isAttacking = false;
+    }
 
 }
